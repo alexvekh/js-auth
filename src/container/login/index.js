b5c160ff5f8affb3de867b8211f61cc986b5868e
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-09-18 22:24:07 -0400

    login, logout pages

--- src/container/login/index.js
+++ src/container/login/index.js
@@ -4,19 +4,11 @@ class SignupForm extends Form {
   FIELD_NAME = {
     EMAIL: 'email',
     PASSWORD: 'password',
-    PASSWORD_AGAIN: 'passwordAgain',
-    ROLE: 'role',
-    IS_CONFIRM: 'isConfirm',
   }
   FIELD_ERROR = {
     IS_EMPTY: 'Введіть значення в поле',
     IS_BIG: 'Дуже довге значення, приберіть зайве',
     EMAIL: 'Ведіть коректне значення email адреси',
-    PASSWORD:
-      'Пароль повинен складатися з не менш ніж 8 символів, включаючи хоча б одну цифруб малу та велику літеру',
-    PASSWORD_AGAIN: 'Ваш другий пароль не збігається з першим',
-    NOT_CONFIRM: 'Ви не погоджуєтесь з правилими',
-    ROLE: 'Ви не обрали роль',
   }
 
   validate = (name, value) => {
@@ -33,30 +25,6 @@ class SignupForm extends Form {
         return this.FIELD_ERROR.EMAIL
       }
     }
-
-    if (name === this.FIELD_NAME.PASSWORD) {
-      if (!REG_EXP_PASSWORD.test(String(value))) {
-        return this.FIELD_ERROR.PASSWORD
-      }
-    }
-
-    if (name === this.FIELD_NAME.PASSWORD_AGAIN) {
-      if (String(value) !== this.value[this.FIELD_NAME.PASSWORD]) {
-        return this.FIELD_ERROR.PASSWORD_AGAIN
-      }
-    }
-
-    if (name === this.FIELD_NAME.ROLE) {
-      if (isNaN(value)) {
-        return this.FIELD_ERROR.ROLE
-      }
-    }
-
-    if (name === this.FIELD_NAME.IS_CONFIRM) {
-      if (Boolean(value) !== true) {
-        return this.FIELD_ERROR.NOT_CONFIRM
-      }
-    }
   }
 
   submit = async () => {
@@ -67,7 +35,7 @@ class SignupForm extends Form {
       this.setAlert('progress', 'Завантаження...')
 
       try {
-        const res = await fetch('/signup', {
+        const res = await fetch('/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -94,13 +62,15 @@ class SignupForm extends Form {
     return JSON.stringify({
       [this.FIELD_NAME.EMAIL]: this.value[this.FIELD_NAME.EMAIL],
       [this.FIELD_NAME.PASSWORD]: this.value[this.FIELD_NAME.PASSWORD],
-      [this.FIELD_NAME.ROLE]: this.value[this.FIELD_NAME.ROLE],
     })
   }
 }
 
 window.signupForm = new SignupForm()
 
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.session) location.assign('/')
+})
 
 // other way: (id="")
 // document.addEventListener("DOMContentLoaded", () => {})
